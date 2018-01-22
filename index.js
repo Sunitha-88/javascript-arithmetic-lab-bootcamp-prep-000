bae9d101e8aca7941c5d782c2ce185aa06ff7c47
Suni <suni.suni571@gmail.com>
2018-01-22 15:29:58 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,43 @@
+  
+ 
+	 var a,b;
+	function add(a,b){
+	 
+	  a = Math.floor(Math.random() * 1000);
+    b = Math.floor(Math.random() * 1000);
+		return a+b;
+		}
+		
+	function subtract(a,b){
+	 
+	  a = Math.floor(Math.random() * 1000);
+    b = Math.floor(Math.random() * 1000);
+		return a-b;
+		}
+	function multiply(a,b){
+	 
+	  a = Math.floor(Math.random() * 1000);
+    b = Math.floor(Math.random() * 1000);
+		return a*b;
+		}
+	function divide(a,b){
+	 
+	  a = Math.floor(Math.random() * 1000);
+    b = Math.floor(Math.random() * 1000);
+		return a/b;
+		}
+	function inc(n){
+		return n+1;
+		}
+	function dec(n){
+		return n-1;
+		}
+
+  function makeInt(n){
+		return parseInt("n", 10);
+		}
+	makeInt('sldkjflksjf');
+  function preserveDecimal(n){
+		return parseFloat("n");
+		}
+	preserveDecimal("2.222");
